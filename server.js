0c39fbc595c66fcab8bb3d1f70f3caf4971de776
daveclinton <clintondavid46@gmail.com>
2023-02-22 14:59:27 +0300

    Add credentials

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, 5000, console.log(`Server listening on port ${PORT}`));
+
+// MongoDB Password
+// username ` dave`
+// password `4uAH0ks3ScJMj3zA`
