17fd420d548fff3aacc9b57ae965b054cb805faf
daveclinton <clintondavid46@gmail.com>
2023-02-22 14:02:13 +0300

    Create express server

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 const express = require("express");
 // server
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, 5000, console.log(`Server listening on port ${PORT}`));
