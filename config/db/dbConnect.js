63fbab151469cf10fdd11ccdfb4fb81746061c7c
daveclinton <clintondavid46@gmail.com>
2023-02-22 15:26:36 +0300

    Add mongodb database

--- config/db/dbConnect.js
+++ config/db/dbConnect.js
@@ -1,1 +1,17 @@
 const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect("Connection string", {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: true,
+    });
+    console.log("Connected to mongo");
+  } catch (error) {
+    console.log(`Error, ${error.message}`);
+  }
+};
+
+module.exports = dbConnect;
