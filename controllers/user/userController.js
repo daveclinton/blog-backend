50400c6df95153192cb8395e86d8ac12ecd2f2d0
daveclinton <clintondavid46@gmail.com>
2023-02-27 10:53:59 +0300

    Add try catch statement

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -4,6 +4,16 @@ const User = require("../../models/user/User");
 
 const userRegisterController = async (req, res) => {
   // business logic
-  resizeBy.json({ user: "User Registered" });
+
+  try {
+    const user = await User.create({
+      firstName: "David",
+      lastName: "Clinton",
+      email: "david@example.com",
+      password: "123456789",
+    });
+    res.json(user);
+  } catch (error) {}
+  res.json(error);
 };
 module.exports = { user: userRegisterController };
