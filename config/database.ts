e7278802e92e93f624b6ffaaf64162bb543b7902
daveclinton <clintondavid46@gmail.com>
2023-08-07 17:45:53 +0300

    feat: Change database to vercel db

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("PGHOST"),
+      host: env("POSTGRES_HOST"),
       port: env.int("PGPORT"),
-      database: env("PGDATABASE"),
-      user: env("PGUSER"),
-      password: env("PGPASSWORD"),
+      database: env("POSTGRES_DATABASE"),
+      user: env("POSTGRES_USER"),
+      password: env("POSTGRES_PASSWORD"),
       schema: env("DATABASE_SCHEMA"), // Not required
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
